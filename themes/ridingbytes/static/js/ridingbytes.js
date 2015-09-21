394dee56a29fbd1c81d708b243a3e49955157293
Ramon Bartl <rb@ridingbytes.com>
2015-09-21 23:23:42 +0200

    added blog section

--- themes/ridingbytes/static/js/ridingbytes.js
+++ themes/ridingbytes/static/js/ridingbytes.js
@@ -1,11 +1,13 @@
 jQuery(document).ready(function () {
     'use strict';
 
-
+    // Smooth scrolling
     jQuery('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
+        var href = $anchor.attr('href');
+        href = href.replace("/", "");
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $(href).offset().top
         }, 1500);
         event.preventDefault();
     });
